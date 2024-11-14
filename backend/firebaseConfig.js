d06ada9322e6ecd52f995293c39a056633526de1
Thandekaportiap <thandekaportiap05@gmail.com>
2024-11-14 14:39:33 +0200

    fix: firebase.config

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -1,6 +1,10 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+import admin from 'firebase-admin';
+import { readFileSync } from 'fs';
+import path from 'path';
 
+const serviceAccount = JSON.parse(
+  readFileSync(path.resolve('serviceAccountKey.json'), 'utf8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
